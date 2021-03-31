95e2f2446ec061b540748b85c0b2e99b4a05b665
Vinícius Umetsu <66230638+ViniUme@users.noreply.github.com>
2021-03-30 23:51:53 -0300

    vou fazer algo mais simples

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,13 @@
-numberDecimal = input.nextInt('#decimalText')
-document.write(numberDecimal)
+function calcular(){
+
+    var numberBinario = window.document.getElementById('binarioText')
+    var numberDecimal = window.document.getElementById('decimalText')
+    var valor2 = Number(numberBinario.value)
+    var valor10 = Number(numberDecimal.value)
+    var calculator
+
+    for (let i = -1; i < 0;){ 
+        calculator = valor10 / 2
+        
+    }
+}
