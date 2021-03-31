198b5a60be8becff419ba22d59a14fa595ab2a27
Vinícius Umetsu <66230638+ViniUme@users.noreply.github.com>
2021-03-31 01:23:26 -0300

    Consegui
    me esforcei um pouco mais, e consegui fazer funcionar, não tudo, mas o que me incomodava ja consegui resolver

--- calculadora.js
+++ calculadora.js
@@ -1,13 +1,8 @@
 function calcular(){
 
     var numberBinario = window.document.getElementById('binarioText')
-    var numberDecimal = window.document.getElementById('decimalText')
     var valor2 = Number(numberBinario.value)
-    var valor10 = Number(numberDecimal.value)
-    var calculator
-
-    for (let i = -1; i < 0;){ 
-        calculator = valor10 / 2
-        
-    }
+    var decimal = parseInt(valor2,2)
+    
+    console.log(decimal)
 }
